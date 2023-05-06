a7700b0ff28a0c1664b80ccaea62e0b9bcfc88ee
Nikita <nikita.osminin@urfu.me>
2023-05-06 23:32:46 +0500

    email validator refactored

--- src/app/shared/validators/CustomValidators.ts
+++ src/app/shared/validators/CustomValidators.ts
@@ -3,18 +3,15 @@ import {AbstractControl, FormControl} from "@angular/forms";
 export class CustomValidators{
 
   public static emailValidator(control: FormControl<string>){
-    const regExpAllowUpperAndDigits = /^[a-zA-Z0-9.]+$/;
-    const regExpBanUpper = /^[a-z.]+$/;
-    const regExpOnlyLowerSymbols = /^[a-z]+$/;
+    const firstSegmentAllowedSymbols = /^[a-zA-Z0-9,.`/\-_#$%&?*()~;^:\\]+$/;
+    const secondSegmentAllowedSymbols = /^[a-z.]+$/;
     let currentSplitState = control.value.split('@');
     if(currentSplitState.length !== 2)
       return {emailValidator: true};
-    if(!regExpAllowUpperAndDigits.test(currentSplitState[0]) || !regExpBanUpper.test(currentSplitState[1]))
+    if(!firstSegmentAllowedSymbols.test(currentSplitState[0]) || !secondSegmentAllowedSymbols.test(currentSplitState[1]))
       return {emailValidator: true};
     currentSplitState = currentSplitState[1].split('.');
-    if(currentSplitState.length !== 2)
-      return {emailValidator: true};
-    if(!regExpOnlyLowerSymbols.test(currentSplitState[0]) || !regExpOnlyLowerSymbols.test(currentSplitState[1]))
+    if(currentSplitState.length !== 2 || currentSplitState[1].length === 0)
       return {emailValidator: true};
     return null;
   }
